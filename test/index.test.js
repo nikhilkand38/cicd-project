67ab3e600ea0c781165cd604a2474bfef264d7ee
Nikhil <kandukurinikhil18@gmail.com>
2026-02-14 17:41:31 +0530

    new

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,12 @@
 const request = require("supertest");
 const app = require("../src/index");
 
-describe("App Test", () => {
-    test("GET / should return message", async () => {
-        const res = await request(app);
-        expect(res.body.message).toBe("Node Automation Demo Running");
-    });
-});
+describe("Index Route Tests", () => {
 
+  test("GET / should return message", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe("Node Automation Demo Running");
+  });
+
+})
